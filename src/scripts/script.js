4c69f59d86c4827942fc32186dd3ede7c2318a69
David Daza <drdaza18@gmail.com>
2022-11-09 20:42:24 +0100

    funtional likebutton

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,29 @@
 function modification(){
     var LikeButton = document.getElementById("like-button");
-    LikeButton.style.color= "gray";
+    var aux= LikeButton.style.color;
+    if(aux=="")
+    {
+        console.log("activo");
+        LikeButton.style.color= "brown";
+        LikeButton.style.transition="all 0.5s"
+    }
+    else if(aux=="brown")
+    {
+        console.log("dislike");
+        LikeButton.style.color= "gray";
+    }
+    else if(aux=="gray")
+    {
+        console.log("like")
+        LikeButton.style.color="brown";
+    }
+
 }
-function load(){
-    let LikeButton = document.getElementById("like-button");
-    LikeButton.addEventListener("click", modification, false);
+
+function load() {
+        let LikeButton = document.getElementById("like-button");
+        console.log("entra");
+        LikeButton.addEventListener("click", modification, false);
+
 }
 document.addEventListener("DOMContentLoaded",load,false);
