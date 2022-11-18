54a08753fbd0d8a3d2f7b2b450f77cc40fbfd510
David Daza <drdaza18@gmail.com>
2022-11-18 20:27:11 +0100

    create the print info on click algoritm

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -26,13 +26,53 @@ function load() {
 
 }
 document.addEventListener("DOMContentLoaded",load,false);
+/* How print text in a div when clicked an object
+* i create an array with the sentences that the 
+program will print.
+* i accessed at the container of objects (for id)
+* i accessed at the childs of the container
+* (when i click any of childs i targeted it its id )
+* i create and assing an array of the sentences
+* i check the id of the child
+* i accessed at the space from i will print the info
+* print the info  
+*/
 function testEvents(){
     let list = document.getElementById("first");
-    list.childNodes.forEach((element) => {
+    list.addEventListener("click", (e)=> {
+        let idElement=e.target.id
+        if(e.target.classList!=""){
+            let sentences = createSentences();
+            console.log(sentences);
+            checkId(idElement,sentences);
+    }
+    })
+    /* list.childNodes.forEach((element) => {
         element.addEventListener("click", (e)=>{
             e.target.classList.toggle("activo");
         })
-    });
+    }); */
     
+}
+function checkId(param, array){
+    console.log(param);
+    console.log(array);
+    let view= array[param-1];
+    printResult(view);
+    /* array.forEach(element => {
+        console.log(element.index);
+        if(param==element){
+            console.log(param);
+            printResult(element);
+        }
+    }); */
+}
+function printResult(param2){
+    let acces= document.getElementById("text");
+    acces.innerHTML = `<h1>${param2}</h1>`
+}
+function createSentences(){
+    const sentences = ["kirk", "james", "lee", "ulsrich"];
+    return sentences;
 }
 testEvents();
